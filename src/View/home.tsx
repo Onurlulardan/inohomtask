87859812eb3d52c0ee8e05792a938d8ff8df78ee
Onurlulardan <altuntasonur@outlook.com.tr>
2022-07-28 11:41:31 +0300

    data

--- src/View/home.tsx
+++ src/View/home.tsx
@@ -2,13 +2,75 @@ import React from 'react';
 import Data from '../data/data.json';
 
 interface Idata {
-    name: string;
-    icon: string;
-    degree: number;
-    onoff: boolean;
+    Id: number;
+    ScenarioType: number;
+    Name: string;
+    Icon: string;
+    OnOrOff: boolean;
+    HomeOrOutdoor: boolean;
+    IncreaseOrDecrease: number;
 }
 
+let MyData: Idata[] = [
+    {   "Id": 0,
+        "ScenarioType": 1,
+        "Name": "Oturma Odası",
+        "Icon": "fa-solid fa-bell",
+        "OnOrOff": true,
+        "HomeOrOutdoor": false,
+        "IncreaseOrDecrease": 0
+     },
+     {   "Id": 1,
+        "ScenarioType": 1,
+        "Name": "Yatak Odası",
+        "Icon": "fa-solid fa-bell",
+        "OnOrOff": false,
+        "HomeOrOutdoor": false,
+        "IncreaseOrDecrease": 0
+     },
+     {   "Id": 2,
+        "ScenarioType": 2,
+        "Name": "Alarm",
+        "Icon": "fa-solid fa-bell",
+        "OnOrOff": false,
+        "HomeOrOutdoor": true,
+        "IncreaseOrDecrease": 0
+     },
+     {   "Id": 3,
+        "ScenarioType": 3,
+        "Name": "Alarm",
+        "Icon": "fa-solid fa-bell",
+        "OnOrOff": false,
+        "HomeOrOutdoor": true,
+        "IncreaseOrDecrease": 0
+     },
+     {   "Id": 4,
+        "ScenarioType": 4,
+        "Name": "Alarm",
+        "Icon": "fa-solid fa-bell",
+        "OnOrOff": false,
+        "HomeOrOutdoor": true,
+        "IncreaseOrDecrease": 0
+     },
+     {   "Id": 5,
+        "ScenarioType": 5,
+        "Name": "Alarm",
+        "Icon": "fa-solid fa-bell",
+        "OnOrOff": false,
+        "HomeOrOutdoor": true,
+        "IncreaseOrDecrease": 0
+     },
+     {   "Id": 6,
+        "ScenarioType": 6,
+        "Name": "Alarm",
+        "Icon": "fa-solid fa-bell",
+        "OnOrOff": false,
+        "HomeOrOutdoor": true,
+        "IncreaseOrDecrease": 0
+     },
+];
 
+console.log(MyData);
 function home() {
   return (
         <div className="item-cover">
@@ -44,114 +106,48 @@ function home() {
                         </div>
                     </div>
                 </div>
-                <div className="control-list-cover">
+                {MyData.map((data,index)=>{
+                    return (
+                        <div key={index} className="control-list-cover">
                     <div className="control-list-item control-list-item-alert">
                         <div className="control-list-name">
-                            <p>Alarm</p>
+                            <p>{data.Name}</p>
                         </div>
                         <div className="control-list-icon">
                             <i className="fa-solid fa-bell"></i>
                         </div>
-                        <div className="control-list-onoff">
+                        {data.ScenarioType === 1 ? (<div className="control-list-onoff">
+                            <button>I</button>
+                            <button>0</button>
+                        </div>) : "" }
+                        {data.ScenarioType === 2 ? (<div className="control-list-onoff">
                             <button>Ev</button>
                             <button>Dış</button>
-                        </div>
-                        <div className="control-list-add">
-                            <button>Ekle</button>
-                        </div>
-                    </div>
-                    <div className="control-list-item control-list-item-bulb">
-                        <div className="control-list-name">
-                            <p>2</p>
-                        </div>
-                        <div className="control-list-icon">
-                            <i className="ri-lightbulb-line"></i>
-                        </div>
-                        <div className="control-list-onoff">
+                        </div>) : "" }
+                        {data.ScenarioType === 3 ? (<div className="control-list-onoff">
                             <button>I</button>
                             <button>0</button>
-                        </div>
-                        <div className="control-list-add">
-                            <button>Ekle</button>
-                        </div>
-                    </div>
-                    <div className="control-list-item control-list-item-bulb">
-                        <div className="control-list-name">
-                            <p>3</p>
-                        </div>
-                        <div className="control-list-icon">
-                            <i className="ri-lightbulb-line"></i>
-                        </div>
-                        <div className="control-list-onoff">
-                            <button>I</button>
-                            <button>0</button>
-                        </div>
-                        <div className="control-list-add">
-                            <button>Ekle</button>
-                        </div>
-                    </div>
-                    <div className="control-list-item control-list-item-valve">
-                        <div className="control-list-name">
-                            <p>Vana</p>
-                        </div>
-                        <div className="control-list-icon">
-                            <i className="ri-contrast-drop-line"></i>
-                        </div>
-                        <div className="control-list-onoff">
-                            <button>I</button>
-                            <button>0</button>
-                        </div>
-                        <div className="control-list-add">
-                            <button>Ekle</button>
-                        </div>
-                    </div>
-                    <div className="control-list-item control-list-item-combi">
-                        <div className="control-list-name">
-                            <p>Panel <br/> Kombi</p>
-                        </div>
-                        <div className="control-list-icon">
-                            <i className="fa-solid fa-temperature-high"></i>
-                        </div>
-                        <div className="control-list-onoff contol-list-incdec">
+                        </div>) : "" }
+                        {data.ScenarioType === 4 ? (<div className="control-list-onoff contol-list-incdec">
                             <button>-</button>
                             <p>25.0</p>
                             <button>+</button>
-                        </div>
-                        <div className="control-list-add">
-                            <button>Ekle</button>
-                        </div>
-                    </div>
-                    <div className="control-list-item control-list-item-window">
-                        <div className="control-list-name">
-                            <p>Perde</p>
-                        </div>
-                        <div className="control-list-icon">
-                            <i className="ri-window-2-line"></i>
-                        </div>
-                        <div className="control-list-onoff contol-list-incdec">
+                        </div>) : "" }
+                        {data.ScenarioType === 5 ? (<div className="control-list-onoff contol-list-incdec">
                             <button type="button"><i className="ri-arrow-up-s-fill"></i></button>
                             <button type="button" disabled><i className="fa-solid fa-equals"></i></button>
                             <button type="button"><i className="ri-arrow-down-s-fill"></i></button>
-                        </div>
-                        <div className="control-list-add">
-                            <button>Ekle</button>
-                        </div>
-                    </div>
-                    <div className="control-list-item control-list-item-garage">
-                        <div className="control-list-name">
-                            <p>Garaj</p>
-                        </div>
-                        <div className="control-list-icon">
-                            <i className="fa-solid fa-warehouse"></i>
-                        </div>
-                        <div className="control-list-onoff contol-list-incdec">
+                        </div>) : "" }
+                        {data.ScenarioType === 6 ? (<div className="control-list-onoff contol-list-incdec">
                             <button type="button" className="garage-text"><p>Çağır</p></button>
-                        </div>
+                        </div>) : "" }
                         <div className="control-list-add">
                             <button>Ekle</button>
                         </div>
                     </div>
                 </div>
+                    )
+                })}
             </div>
             <div className="item-right">
                 <div className="contol-list-show-cover">
